ed8f6a9ebb981e0f388536d60373a8c80c3cb80e
Adel Chelabi <adimino96@gmail.com>
2024-02-21 18:58:24 +0100

    Doc: Redirect to doc page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import taskRoutes from "./taskRoutes";
 import userRouter from "./userRoutes";
 import swaggerUi from "swagger-ui-express";
@@ -9,9 +9,5 @@ const router = Router();
 router.use("/tasks", taskRoutes);
 router.use("/users", userRouter);
 router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-router.get('/', (req: Request, res: Response) => {
-  res.redirect('/api/v1/api-docs');
-});
 
 export default router;
